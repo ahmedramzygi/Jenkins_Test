cbf8dff25bcae00177ebe20d5d16e3da7882e7fe
Ahmed Ramzy <aeramzy9@gmail.com>
2025-12-24 17:09:40 +0200

    fix

--- SharedLibrary/vars/dockerLogin.groovy
+++ SharedLibrary/vars/dockerLogin.groovy
@@ -2,6 +2,6 @@
 
 def call() {
     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh "echo \$PASS | docker login -u \$USER --password-stdin"
     }
 }
